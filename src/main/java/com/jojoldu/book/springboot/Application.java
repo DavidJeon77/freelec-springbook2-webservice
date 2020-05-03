be8c13cdeeff5182b95aa2d370c1d21aa2a5ef0a
David Jeon <tigerbj7@gmail.com>
2020-05-04 01:13:17 +0900

    스프링시큐리티와 OAuth2.0으로 로그인 기능 구현

--- src/main/java/com/jojoldu/book/springboot/Application.java
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -2,9 +2,8 @@ package com.jojoldu.book.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
+//@EnableJpaAuditing
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
